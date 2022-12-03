9d474ae43e444d1058962a2983eaed9733a4bc92
jmahajan117 <jay.mahajan33@gmail.com>
2022-12-03 00:47:36 -0600

    Finished models

--- DL/dataloader.py
+++ DL/dataloader.py
@@ -1,9 +1,13 @@
 import torch
-import torchaudio
 from torch.utils.data.dataloader import Dataset
-import torch.nn as nn
+import torchaudio
 from os import listdir
 from os.path import isfile, join
+import random
+from torchvision.transforms import transforms
+from PIL import Image
+import numpy as np
+
 
 
 
@@ -16,8 +20,27 @@ class DataLoader(Dataset):
     
 
     def loadData(self, soundsPath, imagesPath):
-        onlyfiles = [f for f in listdir(soundsPath) if isfile(join(soundsPath, f))]
-        print(onlyfiles)
+        soundFiles = [soundsPath + "/" + f for f in listdir(soundsPath) if isfile(join(soundsPath, f))]
+        soundsWithoutF = [f for f in listdir(soundsPath) if isfile(join(soundsPath, f))]
+        trans = transforms.Compose([transforms.ToTensor(), transforms.Resize(64)])
+        dim_wanted = torch.empty((2, 40, 6613))
+
+        for i in range(len(soundFiles)):
+            sound = soundsWithoutF[i]
+            imageName = sound.split(".")[0]
+            audio, sample_rate = torchaudio.load(soundFiles[i])
+            mfccTrans = torchaudio.transforms.MFCC(sample_rate)
+            audio = mfccTrans(audio)
+            if audio.size() != dim_wanted.size():
+                continue
+            self.sounds.append(audio)
+            image = Image.open(imagesPath + "/" + imageName + ".jpeg")
+            image = np.array(image)
+            image = trans(image)
+            self.images.append(image)
+        print("Loaded Sounds & Images")
+
+
     
     def __len__(self):
         return len(self.images)
@@ -25,4 +48,11 @@ class DataLoader(Dataset):
     def __getitem__(self, index):
         return self.images[index], self.sounds[index]
 
-    
+
+def main():
+    data = DataLoader("./spotify_data/song_clips", "./spotify_data/album_covers")
+    print(len(data))
+
+
+if __name__ == "__main__":
+    main()
--- DL/models.py
+++ DL/models.py
@@ -1,6 +1,7 @@
 import torch
-from gan.spectral_normalization import SpectralNorm
 import torch.nn.functional as F
+import torch.nn as nn
+from torchsummary import summary
 class Discriminator(torch.nn.Module):
     def __init__(self, input_channels=3):
         super(Discriminator, self).__init__()
@@ -8,14 +9,14 @@ class Discriminator(torch.nn.Module):
         
 
         
-        self.conv1 = SpectralNorm(torch.nn.Conv2d(3, 128, kernel_size = 4, stride= 2, padding =1))
-        self.conv2 = SpectralNorm(torch.nn.Conv2d(128, 256, kernel_size = 4, stride = 2, padding=1))
+        self.conv1 =torch.nn.Conv2d(3, 128, kernel_size = 4, stride= 2, padding =1)
+        self.conv2 = torch.nn.Conv2d(128, 256, kernel_size = 4, stride = 2, padding=1)
         self.bn1 =torch.nn.BatchNorm2d(256)
-        self.conv3 = SpectralNorm(torch.nn.Conv2d(256, 512, kernel_size = 4, stride = 2, padding=1))
+        self.conv3 = torch.nn.Conv2d(256, 512, kernel_size = 4, stride = 2, padding=1)
         self.bn2 = torch.nn.BatchNorm2d(512)
-        self.conv4 = SpectralNorm(torch.nn.Conv2d(512, 1024, kernel_size = 4, stride = 2, padding=1))
+        self.conv4 = torch.nn.Conv2d(512, 1024, kernel_size = 4, stride = 2, padding=1)
         self.bn3 = torch.nn.BatchNorm2d(1024)
-        self.conv5 = SpectralNorm(torch.nn.Conv2d(1024, 1, kernel_size = 4, stride = 1, padding = 1))
+        self.conv5 = torch.nn.Conv2d(1024, 1, kernel_size = 4, stride = 1, padding = 1)
         self.leaky_relu = torch.nn.LeakyReLU(0.2) 
          
         
@@ -32,34 +33,63 @@ class Discriminator(torch.nn.Module):
         return x
 
 
-class Generator(torch.nn.Module):
-    def __init__(self, noise_dim, output_channels=3):
-        super(Generator, self).__init__()    
-        self.noise_dim = noise_dim
-        
-        
-        self.conv1 = torch.nn.ConvTranspose2d(self.noise_dim, 1024, kernel_size = 4, stride = 1)
-        self.bn1 = torch.nn.BatchNorm2d(1024)
-        self.conv2 = torch.nn.ConvTranspose2d(1024, 512, kernel_size = 4, stride = 2, padding = 1)
-        self.bn2 = torch.nn.BatchNorm2d(512)
-        self.conv3 = torch.nn.ConvTranspose2d(512, 256, kernel_size = 4, stride = 2, padding = 1)
-        self.bn3 = torch.nn.BatchNorm2d(256)
-        self.conv4 = torch.nn.ConvTranspose2d(256, 128, kernel_size = 4, stride = 2, padding =1 )
-        self.bn4 = torch.nn.BatchNorm2d(128)
-        self.conv5 = torch.nn.ConvTranspose2d(128, 3, kernel_size = 4, stride = 2, padding = 1)
-        self.lstm = torch.nn.LSTM((2,40,6613),10,noise_dim) #change dims here
-        #2x40x6613  
+class Generator(nn.Module):
+    
+    def __init__(self, noise_dim):
+        super().__init__()
 
+        self.noise_dim = noise_dim
+        self.model = nn.Sequential(
+            nn.Linear(noise_dim, 128),
+            nn.ReLU(),
+        )
+
+        self.gen = nn.Sequential(
+            nn.ConvTranspose2d(128, 1024, 4, 1, padding=0),
+            nn.BatchNorm2d(1024),
+            nn.ReLU(),
+            nn.ConvTranspose2d(1024, 512, 4, 2, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.ConvTranspose2d(512, 256, 4, 2, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.ConvTranspose2d(256, 128, 4, 2, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ConvTranspose2d(128, 3, 4, 2, padding=1),
+            nn.Tanh()
+        )
     
     def forward(self, x):
-        x = self.lstm(x)
-        x = F.relu(self.bn1(self.conv1(x)))
-        x = F.relu(self.bn2(self.conv2(x)))
-        x = F.relu(self.bn3(self.conv3(x)))
-        x = F.relu(self.bn4(self.conv4(x)))
-        x = torch.tanh(self.conv5(x))
-        
-        
-        return x
+        x = x.view(-1, self.noise_dim)
+        x = self.model(x)
+        x = x.view(-1, 128, 1, 1)
+        return self.gen(x)
     
 
+def main():
+    # g = Generator(529040)
+    # z = torch.rand((2, 40, 6613))
+    # z = z.flatten()
+    # z = z.unsqueeze(dim = 0)
+    
+    # print(z.size())
+    # out = g(z)
+    # print(out.size())
+    z = torch.rand((10, 2, 40, 6613))
+    
+    g = Generator(529040)
+    out = g(z)
+    print(out.size())
+    print(summary(g, (10, 2, 40, 6613), device="cpu"))
+
+
+
+
+
+
+
+
+
+if "__main__" == __name__:
+    main()
